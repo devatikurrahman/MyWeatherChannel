af9e9d00a1f4bddf5cdbb05e5271944bd267c87b
Atikur Rahman <mratikurrahman@gmail.com>
2023-10-04 15:47:20 +0600

    MyWeatherChannel app just created

---
+++ MyWeatherChannel/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyWeatherChannel
+//
+//  Created by Atikur Rahman on 10/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyWeatherChannel/MyWeatherChannelApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyWeatherChannelApp.swift
+//  MyWeatherChannel
+//
+//  Created by Atikur Rahman on 10/4/23.
+//
+
+import SwiftUI
+
+@main
+struct MyWeatherChannelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
